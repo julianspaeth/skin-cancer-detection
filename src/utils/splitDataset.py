657a93bb776abe7976112ec5cba23382379ae308
Julian Späth <spaethju@posteo.de>
2017-12-07 11:56:52 +0100

    Add final datasets

--- src/utils/splitDataset.py
+++ src/utils/splitDataset.py
@@ -1,20 +1,21 @@
 import numpy as np
 import argparse
+import glob
+import os
 
 # splits a dataset to training (60%), test (20%) and validation (20%)
 
-
 def main(args):
-
-    path = args.datapath
 
     # TODO paths
     out_path_training = '/Users/spaethju/Desktop/training.dataset'
     out_path_test = '/Users/spaethju/Desktop/test.dataset'
     out_path_validation = '/Users/spaethju/Desktop/validation.dataset'
 
-    with open(path, 'r') as f:
-        lists = list(f)
+    input_list = glob.glob(os.path.expanduser(args.datapath))
+    lists = []
+    for l in input_list:
+        lists.append(l.split("/")[-1])
 
     x = np.asarray(lists, dtype=np.string_)
     split_index = int(len(x)/2)
@@ -28,15 +29,15 @@ def main(args):
 
     with open(out_path_training, 'w') as f:
         for fn in training:
-            f.write(str(fn) + "\n")
+            f.write(str(fn).replace("b", "").replace("'", "") + "\n")
 
     with open(out_path_test, 'w') as f:
         for fn in test:
-            f.write(str(fn) + "\n")
+            f.write(str(fn).replace("b", "").replace("'", "") + "\n")
 
     with open(out_path_validation, 'w') as f:
         for fn in validation:
-            f.write(str(fn) + "\n")
+            f.write(str(fn).replace("b", "").replace("'", "") + "\n")
 
 
 if __name__ == '__main__':
