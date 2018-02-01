25ce1e03efd5c34d59c49e1e0443f37878a86fe7
Julian Späth <spaethju@posteo.de>
2018-02-01 16:23:11 +0100

    Init app

---
+++ src/app/Melanoma/app/src/main/java/com/example/speilo/melanoma/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.speilo.melanoma;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        PackageManager pm = context.getPackageManager();
+
+        if (pm.hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            Button scanBu = (Button) findViewById(R.id.scanBu);
+            scanBu.setEnabled(true);
+        }
+    }
+}
