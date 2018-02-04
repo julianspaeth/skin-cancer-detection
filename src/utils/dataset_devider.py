507455a28d852d95cf8086d4b20ab74e4c4e70b6
Jonas <jonas.einig@web.de>
2018-02-04 16:31:20 +0100

    finished dataset devider

--- src/utils/dataset_devider.py
+++ src/utils/dataset_devider.py
@@ -5,31 +5,33 @@ import shutil
 dataset_local_path = "/data/scratch/einig/SkinCancerData"
 
 
-prefix_test = '/test'
-dataset_path_test = '../datasets/test.dataset'
+prefix = '/test'
+# dataset_path = '../datasets/training.dataset'
+# dataset_path = '../datasets/validation.dataset'
+dataset_path = '../datasets/test.dataset'
 
-if not os.path.exists(dataset_local_path + prefix_test):
-    os.makedirs(dataset_local_path + prefix_test)
+if not os.path.exists(dataset_local_path + prefix):
+    os.makedirs(dataset_local_path + prefix)
 
-if not os.path.exists(dataset_local_path + prefix_test + "/Images"):
-    os.makedirs(dataset_local_path + prefix_test + "/Images")
+if not os.path.exists(dataset_local_path + prefix + "/Images"):
+    os.makedirs(dataset_local_path + prefix + "/Images")
 
-if not os.path.exists(dataset_local_path + prefix_test + "/Descriptions"):
-    os.makedirs(dataset_local_path + prefix_test + "/Descriptions")
+if not os.path.exists(dataset_local_path + prefix + "/Descriptions"):
+    os.makedirs(dataset_local_path + prefix + "/Descriptions")
 
-with open(os.path.expanduser(dataset_path_test)) as f:
+with open(os.path.expanduser(dataset_path)) as f:
     for line in f:
         src_path = dataset_local_path + "/Images/" + line.strip() + "_resized.jpg"
-        dest_path = dataset_local_path + prefix_test + "/Images/" + line.strip() + "_resized.jpg"
+        dest_path = dataset_local_path + prefix + "/Images/" + line.strip() + "_resized.jpg"
 
         shutil.move(src=src_path, dst=dest_path)
 
         src_path = dataset_local_path + "/Descriptions/" + line.strip()
-        dest_path = dataset_local_path + prefix_test + "/Descriptions/" + line.strip()
+        dest_path = dataset_local_path + prefix + "/Descriptions/" + line.strip()
 
         shutil.move(src=src_path, dst=dest_path)
 
-        exit(0)
+
 
 
 
