7ab8751df9d74d415b15d46c21d4592dd13fa143
Jonas <jonas.einig@web.de>
2018-02-04 16:17:10 +0100

    script for sorting the datafiles into test train and val folders -- not tested yet

---
+++ src/utils/dataset_devider.py
@@ -0,0 +1,30 @@
+import os
+
+import shutil
+
+dataset_local_path = "D:/Data/Documents/AutomaticSaveToDisc/Datasets/ISIC-Archive-Downloader-master/Data"
+
+
+prefix_test = '/test'
+dataset_path_test = '../datasets/test.dataset'
+
+if not os.path.exists(dataset_local_path + prefix_test):
+    os.makedirs(dataset_local_path + prefix_test)
+
+with open(os.path.expanduser(dataset_path_test)) as f:
+    for line in f:
+        src_path = dataset_local_path + "/Images/" + line.strip() + "_resized.jpg"
+        dest_path = dataset_local_path + prefix_test + "/Images/" + line.strip() + "_resized.jpg"
+
+        shutil.move(src=src_path, dst=dest_path)
+
+        src_path = dataset_local_path + "/Descriptions/" + line.strip()
+        dest_path = dataset_local_path + prefix_test + "/Descriptions/" + line.strip()
+
+        shutil.move(src=src_path, dst=dest_path)
+
+        exit(0)
+
+
+
+
