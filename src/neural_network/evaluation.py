acb98260c8df9a3aff9ac4ef8deed8ba9d009c89
Jonas <jonas.einig@web.de>
2018-02-19 14:13:16 +0100

    debug output in evaluation

--- src/neural_network/evaluation.py
+++ src/neural_network/evaluation.py
@@ -103,7 +103,7 @@ def evaluate(img_path=None, snapshot_folder=None, eval_path=None):
                 result_set[0] = 0
                 result_set[1] = 0
 
-            label_set = set(label[0])
+            label_set = label[0]
             print(label_set)
             other = 0
 
