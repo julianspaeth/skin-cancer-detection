e79c3775a65bb7d13e67d52ca67f9b160f646ce7
Julian Späth <spaethju@posteo.de>
2018-02-25 19:55:45 +0100

    Change error handling

--- src/neural_network/evaluation.py
+++ src/neural_network/evaluation.py
@@ -167,6 +167,9 @@ def evaluate(img_path=None, snapshot_folder=None, eval_path=None, verbose=False)
             if i % 1000 == 0:
                 try:
                     print("1000")
+                    print(eval_list_pred)
+                    print(eval_list_test)
+                    print(eval_path + "roc_curve_" + str(i) + ".png")
                     roc_functions.plotROC(pred_labels=eval_list_pred, test_labels=eval_list_test,
                                           save_path=eval_path + "roc_curve_" + str(i) + ".png")
                 except:
