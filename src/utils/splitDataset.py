e09765f0a6416d7c2c0d8cde898e789712dced74
Julian Späth <spaethju@posteo.de>
2018-03-09 20:37:49 +0100

    New datasets

--- src/utils/splitDataset.py
+++ src/utils/splitDataset.py
@@ -3,22 +3,25 @@ import argparse
 import glob
 import os
 
-# splits a dataset to training (50%), test (25%) and validation (25%)
+# splits a dataset to training (60%), test (20%) and validation (20%)
 
-def main(args):
+def main():
 
     # TODO paths
+    datapath = '/Users/spaethju/Downloads/all_des.dataset'
     out_path_training = '/Users/spaethju/Desktop/training.dataset'
     out_path_test = '/Users/spaethju/Desktop/test.dataset'
     out_path_validation = '/Users/spaethju/Desktop/validation.dataset'
 
-    input_list = glob.glob(os.path.expanduser(args.datapath))
-    lists = []
-    for l in input_list:
-        lists.append(l.split("/")[-1])
+    with open(datapath, "r") as input_set:
+        lists = []
+        for fn in input_set:
+            lists.append(fn.replace("\n","").replace("\\", "").replace(".", ""))
 
+    print(lists)
     x = np.asarray(lists, dtype=np.string_)
-    split_index = int(len(x)/2)
+    split_index = int(len(x)/(5/4))
+    print(split_index)
 
     np.random.shuffle(x)
     training, rest = x[:split_index], x[split_index:]
@@ -41,6 +44,7 @@ def main(args):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('datapath', help='glob regex path of the data for the new data set')
-    main(parser.parse_args())
+   # parser = argparse.ArgumentParser()
+    #parser.add_argument('datapath', help='glob regex path of the data for the new data set')
+    #main(parser.parse_args())
+    main()
