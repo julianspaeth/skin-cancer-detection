36d92777e3d06322cf7996309f951c89e9d5bd6c
Jonas <jonas.einig@web.de>
2018-03-09 21:27:51 +0100

    remove debug outputs

--- src/neural_network/train_network.py
+++ src/neural_network/train_network.py
@@ -92,16 +92,10 @@ def train(img_path, loss_func, learning_rate, batch_size, snapshot_folder, save_
 
     ## Start Preprocessing and Augmentation --------------------------------
     x_preprocessed = preprocess(x)
-    print("Debug : Xpreprocessed : {}".format(x_preprocessed))
     # Flip and rotate augmentations with to 20 %
     x_augmented = augment(x_preprocessed, random=True, percentage=20, rotation=True, vertical_flip=True, horizontal_flip=True)
-    print("Debug : x_augmentes : {}".format(x_augmented))
-
     # Other augmentations just 5 %
-
     x_augmented_rare = augment(x_augmented, random=True, percentage=5, brightness=True, contrast=True, hue=True, saturation=True)
-    print("Debug : x_augmented_rare : {}".format(x_augmented_rare))
-
     ## End Preprocessing and Augmentation --------------------------------
 
     net, endpoints = inception_v3.inception_v3(inputs=x_augmented_rare, num_classes=2, is_training=True,
